c5ee3d562ec97c402264a045cb9a71b1e2dd159c
lucas-candeias <lucas-candeias@outlook.com>
2024-05-08 21:03:23 -0300

    Js002

---
+++ JS/JS-02-Variaveis/JS02-ExercicioCasa/main.js
@@ -0,0 +1,8 @@
+let nota1 = 10
+let nota2 = 0
+let nota3 = 8
+let nota4 = 7
+let media = nota1 + nota2 + nota3 + nota4
+let notaFinal = media / 4
+
+console.log('Sua nota final é: ' + notaFinal)
---
+++ JS/JS-02-Variaveis/main.js
@@ -0,0 +1,33 @@
+// console.log('Hello World!');
+
+// variaveis
+// quero criar um espaço na memoria do computador chamado _felippe e dizer que esse espaço na memoria irá receber o valor 8
+
+
+
+// let letsGo = 4
+// letsGo = 6
+
+// console.log(letsGo)
+
+
+// // const
+// const PI = 3.14
+
+// console.error(PI)
+
+let nota1 = 10
+let nota2 = 0
+let nota3 = 8
+let nota4 = 7
+let media = nota1 + nota2 + nota3 + nota4
+let notaFinal = media / 4
+
+console.log(notaFinal)
+
+
+
+
+
+
+
