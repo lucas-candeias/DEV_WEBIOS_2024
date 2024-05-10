530f56ce2d009db5d44f3e14215745f47425e96d
lucas-candeias <lucas-candeias@outlook.com>
2024-05-09 21:32:57 -0300

    fimAula02

--- JS/JS-02-Variaveis/main.js
+++ JS/JS-02-Variaveis/main.js
@@ -40,6 +40,52 @@ let num2 = "1"
 
 console.log(num1 == num2)
 
+console.clear()
+
+var operador1 = 8
+var operador2 = null
+
+console.log(operador1 * operador2)
+
+console.clear()
+
+var operador1 = 8
+var operador2 = null
+console.log(operador1 * operador2) // 8 * null = 0
+
+var operador3 = "5"
+var operador4 = 1
+console.log(operador3 - operador4) //"5" - 1 = 4
+
+var operador5 = "5"
+var operador6 = 1
+console.log(operador5 + operador6) // "5" + "1" = 51
+
+var operador7 = "five"
+var operador8 = 2
+console.log(operador7 * operador8) // "five = 5" * 2
+
+console.log()
+
+var valor1 = 2
+var valor2 = 2
+var resultado = valor1 + valor2
+
+console.log("resultado é " + resultado)
+
+console.clear()
+
+let valor3 = 4
+let valor4 = 6
+let resultado1 = valor3 > valor4 && valor3 == valor4
+
+console.log(resultado1)
+
+console.clear()
 
 
+let valor5 = 4
+let valor6 = 6
+let resultado2 = valor5 < valor6 || valor5 == valor6
 
+console.log(resultado2)
