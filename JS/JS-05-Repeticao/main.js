d10581401a5b387dd8de405a0b737666f54730f0
lucas-candeias <lucas-candeias@outlook.com>
2024-05-15 21:40:51 -0300

    aula05

---
+++ JS/JS-05-Repeticao/main.js
@@ -0,0 +1,39 @@
+//WHILE
+//ENQUANTO 0 < 10 vou apresentar a mensagem "0 ainda é menor que 10"  
+
+// var contador = 0
+
+// while (contador < 10) {
+//     console.log("0 ainda é menor que 10");
+//     contador++;
+//     debugger
+// }
+
+// console.clear();
+
+// var numero = prompt("Digite um numero")
+// var multiplicador = 0
+
+
+// while (multiplicador <= 10) {
+//     console.log(alert(numero + "x" + multiplicador + "= " + resultado));
+//     multiplicador++
+//     var resultado = numero*multiplicador
+// }
+
+var contador = 0
+
+do {
+    
+    console.log(contador+ " ainda é menor que 10");
+    contador++;
+} while (contador <10);
+
+console.clear();
+
+for(var contador = 10; contador < 10; contador++){
+    console.log(" ainda é menor que 10");
+} 
+
+
+
