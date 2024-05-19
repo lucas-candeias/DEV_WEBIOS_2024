45c1167ff88d741874b41bd844fb61319ce6a202
lucas-candeias <164963925+lucas-candeias@users.noreply.github.com>
2024-05-19 18:39:04 -0300

    Exercicio03

---
+++ JS/JS-03-Objetivos/exercicioCasa/main.js
@@ -0,0 +1,12 @@
+// Criação da string
+var frase = "Estamos aprendendo JavaScript e seremos futuros Devs";
+
+console.log(frase.charAt(19));
+
+
+console.log(frase.length);
+
+
+console.log(frase.toUpperCase());
+
+console.log(frase.slice(-4));
